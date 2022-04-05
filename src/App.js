a023b94b7a1de9a2105ee53fb9e27953df3ee065
NikolaK12 <96307650+NikolaK12@users.noreply.github.com>
2022-04-05 23:29:59 +0200

    .

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ export default class App extends React.Component {
     messages: [],
     member: {
       username: User(),
-      color: randomColor(),
+      color: randomColor({
+        luminosity: "light",
+      }),
     },
     show: false,
   };
